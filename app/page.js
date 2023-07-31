dd3e6ef110b752a5b9f2f17d404aabb0b6f2a498
Duan Josias Sydor <josiasduan@gmail.com>
2023-07-30 21:55:00 -0300

    Ajustes no layout e codigo

--- app/page.js
+++ app/page.js
@@ -8,40 +8,37 @@ export default function Home() {
   let getPhrase = (event) => {
     setState(event.target.value)
   }
-  let changeRoute = () => {
-    console.log("oi")
-  }
+
   return (
-    <div className='index-container' ba>
+    <div className='index-container'>
       <div className='generator-container'>
         <h1>Gerador de molde de letras</h1>
         <form>
           <div className='input-field'>
             <label>Palavra a ser gerada</label>
-            <input type='text' onChange={getPhrase} />
+            <input type='text' onChange={getPhrase} placeholder='Digite a palavra sem espaços ex:FLORDELOTUS' />
           </div>
-          <div className='input-group'>
+          <div className='input-group' >
             <div className='input-field'>
               <label>Tamanho</label>
-              <input type='number' />
+              <input type='number' disabled />
             </div>
             <div className='input-field'>
               <label>Cor</label>
-              <input>
-
-              </input>
+              <input disabled />
             </div>
             <div className='input-field'>
               <label>Fonte</label>
-              <input />
+              <input disabled />
             </div>
           </div>
+
           <div className='input-field '>
             <Link className='submit' href={`/phrase/${state}`}>Enviar</Link>
           </div>
         </form>
         <span className='insta-link'>
-          By <a hreF="https://www.instagram.com/ssatanael123/">@ssatanael123</a>
+          By <a href="https://www.instagram.com/ssatanael123/">@ssatanael123</a>
         </span>
       </div>
     </div>
